64c6fc657abb06eccf9130321056f256a12ab2a9
arizona95 <arizona95@naver.com>
2022-09-01 16:39:16 +0900

    m

--- lab/graph/Editor/public/simulator.js
+++ lab/graph/Editor/public/simulator.js
@@ -1,7 +1,13 @@
 function test(props){
     console.log(props);
     const exec = require("child_process").exec;
-    var process = exec("cd ..&& cd.. && c:\\users\\arizona95\\Anaconda3\\Scripts\\activate tensorflow && python graph_simulator.py");
+    var args1 = props.graph.replaceAll("\"","$")
+    var args2 = "$"+props.selected
+    var args3 = props.state.replaceAll("\"","$")
+    var args = " "+args1+" "+args2+" "+args3
+    var cmd = "cd ..&& cd.. && c:\\users\\arizona95\\Anaconda3\\Scripts\\activate tensorflow && python graph_simulator_electron.py"+ args
+    console.log(cmd)
+    var process = exec(cmd);
     process.stderr.on("data", function (data) {
       console.error(data.toString());
     });
